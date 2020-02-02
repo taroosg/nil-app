50ad62e99a3d4cf138f6a51abddfb1ae12d2157a
taroosg <macchinetta.xyz@gmail.com>
2020-02-02 17:18:50 +0900

    fix getGrass

--- src/App.jsx
+++ src/App.jsx
@@ -96,7 +96,7 @@ const App = () => {
         setText('');
         document.getElementById('text').value = '';
         getGrass(`${grassUrl}?uid=${user.uid}`).then(result => {
-          setGrass(result.data.reverse());
+          setGrass(result.data);
           setModalOpen(false);
         })
       })
